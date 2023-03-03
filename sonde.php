8f39eea24746959e42b9d4cfadd259f2487670f8
Noone-J <njoseph.ledantec@gmail.com>
2023-03-03 14:47:57 +0100

    V1.0 Sonde

---
+++ sonde.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta charset="utf-8">
+  
+  <!-- importer le fichier de style -->
+  <link rel="stylesheet" type="text/css" href=""/>
+  </head>
+  <body>
+    <div class="titre">
+      <h1 class="titre1">Psycho<span class="titre2">Quizz</span></h1>
+    </div>
+  </body>
+</html>
