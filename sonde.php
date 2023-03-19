0a54cc0f7421f9d9cefd5f8e7c8782ce2636a2ab
Noone-J <123181336+Noone-J@users.noreply.github.com>
2023-03-19 12:29:03 +0100

    Add files via upload

--- sonde.php
+++ sonde.php
@@ -1,48 +1,31 @@
 <?php
-session_start();
-include ("back-end/sonde.php");
-$origins = getOrigins();
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-  
-  
-    <!-- importer le fichier de style -->
-    <link rel="stylesheet" href="assets/css/main.css">
-    <link rel="stylesheet" href="assets/css/sonde.css">
-    <title>Sondage</title>
-    <link rel="icon" type="image/jpg" href="assets/picture/picture-icon.png">
+function getDtbLog(){
 
-</head>
-<body>
-    <div class="titre">
-        <h1 class="titre1">Psycho<span class="titre2">Quizz</span></h1>
-    </div>
-    <form class= "form" method="post" action="<?php createSonde(); ?>">
-        <h2>Qu'elle est votre parcours scolaire ?</h2>
-        <select name="origin">
-        <option value="">Selctionner un parcours</option>
-        <?php
-            foreach ($origins as $origin) {
-            echo "<option value='" . $origin->IDORIGINE . "'>" . $origin->NOM . "</option>";
-            }
-        ?>
-        </select>
-        <h2>Êtes vous une Femme ou un Homme ?</h2>
-        <div class="radio-group">
-            <label class="radio">
-                
-                <input class = "radio" type="radio" value="homme"name="gender"> Homme
-                <span></span>
-            </label>
-            <label class="radio">
-                <input type="radio" value="femme"name="gender"> Femme
-                <span></span>
-            </label>
-        </div><br>
-        <button class ="button" type="submit" name="submit">Faire le quizz !</button>
-    </form> 
-</body>
-</html>
+  require("utilisateur-bdd.php");
+  try {
+      $db = new PDO('mysql:host=' . $host . ';dbname=' . $dbname, $user, $password);
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+      return $db;
+  } catch (PDOException $e) {
+      echo 'Erreur : ' . $e->getMessage() . '</br/>';
+      die();
+  }
+}
+  
+function getOrigins(){
+  $login = getDtblog();
+  $q = $login->query("SELECT NOM,IDORIGINE FROM origine");
+  $origins = $q->fetchAll(PDO::FETCH_CLASS);
+  return $origins;
+}
+
+function createSonde()
+{
+  if (isset($_POST["submit"])) {
+    $login = getDtbLog();
+    $request = $login->query("INSERT INTO sonde(IDORIGINE, ANNEE, SEXE) VALUES (" . $_POST["origin"] . ", " . date("Y") . ", '" . $_POST["gender"] . "')");
+    $idSonde = $login->lastInsertId();
+    $_SESSION["id"] = $idSonde;
+    header("Location: ./questionnaire.php");
+  }
+}
