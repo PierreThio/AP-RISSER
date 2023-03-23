a190ff658b86caa0b28849fb9cc40c9583d8d108
Pierre Thioliere <126860665+PierreThio@users.noreply.github.com>
2023-03-23 19:34:00 +0100

    Add files via upload

--- back-end/questionnaire.php
+++ back-end/questionnaire.php
@@ -2,11 +2,13 @@
 
 function getDatabaseConnexion()
 {
+    // récupération des données de connexion à la BDD
     require("utilisateur-bdd.php");
     try {
         $db = new PDO('mysql:host=' . $host . ';dbname=' . $dbname, $user, $password);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
         return $db;
+        //affichage d'une erreur en cas d'echec de connexion
     } catch (PDOException $e) {
         echo 'Erreur : ' . $e->getMessage() . '</br/>';
         die();
@@ -15,15 +17,21 @@ function getDatabaseConnexion()
 
 function getQuestions()
 {
+    //connexion a la BDD
     $conn = getDatabaseConnexion();
+    // requête pour récupérer les questions
     $request = "SELECT question.IDQUESTION, question.LIBELLE, question.IDTYPEQUESTION, question.IDSCOREFERMEE, question.IDSCORECH, scorefermee.REP, scorefermee.SCOREFRES, scorefermee.SCOREFDEV, scorech.NBPTMULTRES, scorech.NBPTMULTDEV FROM question, scorefermee, scorech WHERE question.IDSCOREFERMEE = scorefermee.IDSCOREF AND question.IDSCORECH = scorech.IDSCORECH ORDER BY question.IDQUESTION";
+    // prépare et exacute la requête
     $statement = $conn->query($request);
-    $question = $statement->fetchAll(PDO::FETCH_CLASS);
-    return $question;
+    // créer une instancie les questions
+    $questions = $statement->fetchAll(PDO::FETCH_CLASS);
+    // retourne les quesitons
+    return $questions;
 }
 function getResults()
 {
     $conn = getDatabaseConnexion();
+    // requête qui récupérer les valeurs des score en réseau et en dev
     $request = "SELECT SUM(VALEURRES) AS TOTALRES, SUM(VALEURDEV) AS TOTALDEV FROM reponseassociee WHERE IDSONDE =" . $_SESSION['id'];
     $statement = $conn->query($request);
     $result = $statement->fetch(PDO::FETCH_ASSOC);
@@ -31,10 +39,16 @@ function getResults()
     $statement = $conn->query($request);
     $notices = $statement->fetchAll(PDO::FETCH_CLASS);
     foreach ($notices as $notice) {
-        if ($notice->BORNEDEV < $result["TOTALDEV"] || $notice->BORNEDEV == 0) {
-            if ($notice->BORNERES < $result["TOTALRES"] || $notice->BORNERES == 0) {
-                Header("Location: results.php?avis=" . $notice->IDAVIS);
-            }
+        if ($notice->BORNEDEV < $result["TOTALDEV"] || $notice->BORNEDEV != 0) {
+            Header("Location: results.php?avis=" . $notice->IDAVIS);
+        }
+        if ($notice->BORNERES < $result["TOTALRES"] || $notice->BORNERES != 0) {
+            
+        }
+        if($notice->BORNERES != 0 && $notice->BORNEDEV != 0){
+            if($notice->BORNEDEV < $result["TOTALDEV"] && $notice->BORNERES < $result["TOTALRES"]){
+            Header("Location: results.php?avis=" . $notice->IDAVIS);
+        }
         }
     }
 }
